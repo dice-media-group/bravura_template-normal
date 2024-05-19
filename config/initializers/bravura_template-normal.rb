4b358af14376bc2548d69f19ff74d59f05340afe
captproton <carl@wdwhub.net>
2024-05-19 16:18:53 -0700

    ADD: blog index, show pages, and supporting partials

--- config/initializers/bravura_template-normal.rb
+++ config/initializers/bravura_template-normal.rb
@@ -1,12 +1,10 @@
-# FIXME: This is a temporary solution to load the template
-ActiveSupport.on_load(:action_controller_base) do
-  before_action do
-    BlogTemplates.register_template(
-      name: 'Normal',
-      gem: 'bravura_template-normal',
-      description: 'A normal template',
-      thumbnail: "bravura_template/normal/thumbnail.webp",
-      preview_link: "https://normal.feather.blog/"
-    )
-  end
-end
+# config/initializers/bravura_template-normal.rb
+require_dependency 'blog_templates'
+
+BlogTemplates.register_template(
+  name: 'Normal',
+  gem: 'bravura_template-normal',
+  description: 'A normal template',
+  thumbnail: "bravura_template/normal/thumbnail.webp",
+  preview_link: "https://normal.feather.blog/"
+)
